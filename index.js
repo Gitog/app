a5039014253d448ea26e9485204299cfed7d74b0
James <jamesnjorio@gmail.com>
2023-02-27 08:21:00 +0300

    Added and trimmedreq url

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ var http = require('http');
 var url = require('url');
 
 //The server should respond to all requesta with a string
-var server = http.createServer(function(re, res){
+var server = http.createServer(function(req, res){
 
     //Get the URL and parse it
-    var parsedurl = url.parse(req.url, true);
+    var parsedUrl = url.parse(req.url, true);
 
     //Get the path
+    var path = parsedUrl.pathname;
+    var trimmedpath = path.replace(/^\/+|\/+$/g,'')
 
     //Send the response
-
-    //Log the request path
-
     res.end("Hello World\n")
+ 
+    //Log the request path
+    console.log('Request received on path: '+trimmedpath)
+
+    
 })
 
 //Start the server, and have it listen  on port 3000
